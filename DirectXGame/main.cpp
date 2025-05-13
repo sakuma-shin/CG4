d8a1686a0a8155afb500c3042504d467290f2579
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-05-13 10:02:54 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,30 @@
 #include <Windows.h>
+#include "kamataEngine.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	KamataEngine::Initialize(L"LE3C_10_サクマ_シン");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 	
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
 	return 0;
 }
