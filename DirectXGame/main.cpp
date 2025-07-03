78e41682b0738ffa7e77eb22eed17ce8263b9fce
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-07-03 15:45:55 +0900

    コメントを消した

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -13,37 +13,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	gameManager->Run();
 
-	//GameScene* gameScene = new GameScene();
-
-	//gameScene->Initialize();
-
-	//// DirectXCommonインスタンスの取得
-	//DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
-	//// DirectXCommonクラスが管理している、ウインドウの幅と高さの取得
-	//int32_t w = dxCommon->GetBackBufferWidth();
-	//int32_t h = dxCommon->GetBackBufferHeight();
-	//DebugText::GetInstance()->ConsolePrintf(std::format("width:{},height: {}\n", w, h).c_str());
-
-	//// メインループ
-	//while (true) {
-	//	// エンジンの更新
-	//	if (KamataEngine::Update()) {
-	//		break;
-	//	}
-
-	//	gameScene->Update();
-
-	//	// 描画開始
-	//	dxCommon->PreDraw();
-
-	//	gameScene->Draw();
-
-	//	// 描画終了
-	//	dxCommon->PostDraw();
-	//}
-	/*delete gameScene;*/
-
 	delete gameManager;
 	// エンジンの終了処理
 	KamataEngine::Finalize();
