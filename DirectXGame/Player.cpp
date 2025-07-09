c29527beac4f966f976f55e057b1175dfa662592
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-07-09 10:28:55 +0900

    自機を動かせるように修正

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -10,10 +10,29 @@ void Player::Initialize(KamataEngine::Model2* model) {
 	worldTransform_.rotation_.y = 2.0f/3.0f*pi;
 	worldTransform_.scale_ = {3.0f, 3.0f, 3.0f};
 	worldTransform_.UpdateMatrix();
+
+	input_ = Input::GetInstance();
 }
 
 void Player::Update() {
+	const float kSpeed = 0.5f;
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.translation_.x -= kSpeed;
+	}
 
+	if (input_->PushKey(DIK_D)) {
+		worldTransform_.translation_.x += kSpeed;
+	}
+
+	if (input_->PushKey(DIK_W)) {
+		worldTransform_.translation_.y += kSpeed;
+	}
+
+	if (input_->PushKey(DIK_S)) {
+		worldTransform_.translation_.y -= kSpeed;
+	}
+
+	worldTransform_.UpdateMatrix();
 }
 
 void Player::Draw(KamataEngine::Camera &camera) { model_->Draw(worldTransform_, camera); }
